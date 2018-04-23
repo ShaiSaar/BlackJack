dbfe637bbc8f2d37f0daaef4419674e38d3b77f8
shaisaar <shaissaar@gmail.com>
2018-04-23 16:17:02 +0300

    1.0.0

---
+++ index.js
@@ -0,0 +1,30 @@
+var readlineSync = require('readline-sync');
+
+var userName = readlineSync.question('Do you want to play BlackJack? yes/no \n');
+
+if (userName=="yes"){
+    var sum = 0;
+    var num =Math.floor(Math.random()*10);
+    sum+=num;
+    while(sum<22){
+        // if(num==1){
+        //     var answer = readlineSync.question('Do you want to play BlackJack? yes/no');
+        // }
+        console.log("You got: "+num);
+        console.log("Your sum total is: "+sum);
+        console.log("");
+        var answer = readlineSync.question("Would you like to get another card? Yes/no \n");
+        if (answer=="no"){
+            console.log("Thank you for playing, you score is "+ sum);
+            return;
+        } else {
+            num =Math.floor(Math.random()*10);
+            sum+=num;
+        }
+    }
+    console.log("OOHHH you lost! your score is "+ sum);
+} else if(userName=="no"){
+    console.log("Chicken!!!!!");
+} else {
+    console.log("It's only yes or no chicken");
+}
